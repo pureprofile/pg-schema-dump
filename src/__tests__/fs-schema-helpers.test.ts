54e0e376a520819e085e4d1f01a25f602eb47bdd
Martin Zagora <zaggino@gmail.com>
2020-07-06 17:01:36 +0200

    feat: add code coverage, run tests with 100%

--- src/__tests__/fs-schema-helpers.test.ts
+++ src/__tests__/fs-schema-helpers.test.ts
@@ -1,4 +1,4 @@
-import { normalizedSrc } from '../fs-schema-helpers';
+import { normalizedSrc, unquoted, quotedIfKeyword, sortedAttributes } from '../fs-schema-helpers';
 
 test('normalizedSrc', () => {
   expect(normalizedSrc(null as any)).toBe(null);
@@ -8,4 +8,53 @@ test('normalizedSrc', () => {
   expect(normalizedSrc(`hello\n\rworld`)).toBe(`hello\nworld`);
   expect(normalizedSrc(`hello\nworld`)).toBe(`hello\nworld`);
   expect(normalizedSrc(`hello\tworld`)).toBe(`hello  world`);
+});
+
+test('unquoted', () => {
+  expect(unquoted(`xxx`)).toBe(`xxx`);
+  expect(unquoted(`"xxx"`)).toBe(`xxx`);
+});
+
+test('quotedIfKeyword', () => {
+  expect(quotedIfKeyword(`count`)).toBe(`"count"`);
+  expect(quotedIfKeyword(`from`)).toBe(`"from"`);
+  expect(quotedIfKeyword(`happy`)).toBe(`happy`);
+});
+
+test('sortedAttributes', () => {
+  expect(
+    sortedAttributes([
+      {
+        name: 'city',
+        references: {
+          table: 'cities',
+        },
+      },
+      {
+        name: 'id',
+      },
+      {
+        name: 'name',
+      },
+      {
+        name: 'created_at',
+      },
+    ] as any)
+  ).toEqual([
+    {
+      name: 'id',
+    },
+    {
+      name: 'created_at',
+    },
+    {
+      name: 'city',
+      references: {
+        table: 'cities',
+      },
+    },
+    {
+      name: 'name',
+    },
+  ]);
 });
