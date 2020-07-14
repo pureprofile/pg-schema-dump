22aa100f9a7d7f075bfe72412c0e513b9b6e7c1d
Martin Zagora <zaggino@gmail.com>
2020-07-14 09:49:02 +0200

    fix: test-case

--- src/__tests__/pg-helpers.test.ts
+++ src/__tests__/pg-helpers.test.ts
@@ -3,18 +3,11 @@ import { sqlGetTableReferences } from '../pg-helpers';
 test('sqlGetTableReferences', () => {
   expect(
     sqlGetTableReferences(`
-    create table acem.acem_config (
-      id bigint not null default nextval('acem.acem_config_id_seq'::regclass) primary key,
+    create table my.table (
+      id bigint not null default nextval('my.id_seq'::regclass) primary key,
       data_source_id bigint not null references data_source,
-      account_id bigint not null,
-      acem_code character varying(10) not null,
-      acem_name character varying(100) not null,
-      domain text,
-      instance_id bigint not null,
-      panel_id bigint not null,
-      platform_id bigint not null,
-      tenant_id bigint not null
+      account_id bigint not null references account
     );  
   `)
-  ).toEqual(['data_source']);
+  ).toEqual(['data_source', 'account']);
 });
